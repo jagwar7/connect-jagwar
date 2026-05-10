d169a606ac3ac51e66bb3b0943cbe3147812477b
Nasim Akhter <93286210+jagwar7@users.noreply.github.com>
2026-05-10 12:01:01 +0530

    Add Cross-Origin-Opener-Policy header
    Allowed origin poups opener

--- next.config.ts
+++ next.config.ts
@@ -30,6 +30,18 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async headers(){
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin-allow-popups"
+          }
+        ]
+      }
+    ]
 };
 
 export default nextConfig;
