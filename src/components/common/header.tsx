a6302a8b941180a3e23329bd6b0d0946cf2e44e4
jagwar7 <com.official.akhter007@gmail.com>
2026-05-14 20:50:13 +0530

    STATE TEST 5

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -61,7 +61,12 @@ export function Header() {
       console.log(error)
     }
   }
-  
+
+
+
+  useEffect(()=>{
+    console.log("User state in header : ", user);
+  }, [user]);
 
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
